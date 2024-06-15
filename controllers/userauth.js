274971e4fd6dc8ce0074fdf87883a9b3f55cfb4d
ajmalcvaju <ajmalmayanad@gmail.com>
2024-06-15 14:14:01 +0530

    next-pagination

--- controllers/userauth.js
+++ controllers/userauth.js
@@ -3,14 +3,17 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     let user = await User.findOne({ email });
+    if(user){
     if (user.is_blocked == 1) {
       res.render("user/login", { blocked: true });
     } else if (user && password === user.password) {
       req.session.email = email;
       res.redirect("/");
-    } else {
-      res.redirect("/");
     }
+  }else{
+    console.log("hi")
+      res.render("user/login",{invalid:true});
+  }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
