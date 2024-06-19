514f5819e76d3b9a430ec498b4b077747114bb7e
ajmalcvaju <ajmalmayanad@gmail.com>
2024-06-19 11:43:16 +0530

    elegance-hosting

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -7,57 +7,39 @@ const adminCategoryPanel = require("../controllers/adminCategoryPanel");
 const adminOrderPanel = require("../controllers/adminOrderPanel");
 const adminCouponPanel = require("../controllers/adminCouponPanel");
 const adminsalesReportPanel = require("../controllers/adminsalesReportPanel");
-const multer = require("multer");
-const path = require("path");
 const middleware = require("../middlewares/middlewares");
 
-router.get("/", middleware.checkSession3, adminController.loginLoad);
 
+router.get("/", middleware.checkSession3, adminController.loginLoad);
 router.get("/adminLogin", adminController.loginLoad);
 router.post("/adminLogin", adminController.verifyLogin);
-
 router.get("/orders", adminOrderPanel.adminOrder);
 router.get("/block-user", adminUserPanel.blockUser);
 router.get("/unblock-user", adminUserPanel.unBlockUser);
-
 router.get("/product", adminProductPanel.adminProduct);
 router.get("/addProduct", adminProductPanel.addProduct);
 router.post("/ProductExist", adminProductPanel.ProductExist);
-
-
 router.post("/addProduct",middleware.productImage,adminProductPanel.updateProduct);
-
 router.get("/user", adminUserPanel.adminUser);
 router.get("/addUser", adminUserPanel.addUser);
-
-
-
 router.post("/addUser", middleware.userImage, adminUserPanel.updateUser);
 router.post("/UserExist", adminUserPanel.UserExist);
-
 router.get("/category", adminCategoryPanel.adminCategory);
 router.post("/CategoryExist", adminCategoryPanel.CategoryExist);
 router.get("/addcategory", adminCategoryPanel.addCategory);
-
 router.post("/addCategory", middleware.categoryImage,adminCategoryPanel.updateCategory);
-
 router.get("/delete-product", adminProductPanel.deleteUser);
 router.get("/restore-product", adminProductPanel.restoreUser);
-
 router.get("/delete-category", adminCategoryPanel.deleteCategory);
 router.get("/restore-category", adminCategoryPanel.restoreCategory);
-
 router.get("/edit-user", adminUserPanel.editUser);
 router.get("/edit-product", adminProductPanel.editProduct);
 router.get("/edit-category", adminCategoryPanel.editCategory);
-
 router.post("/edit-user",middleware.uploadNone,adminUserPanel.updatingUser);
 router.post("/edit-product", adminProductPanel.updatingProduct);
 router.post("/edit-category", adminCategoryPanel.updatingCategory);
-
 router.get("/manage", adminOrderPanel.manageOrder);
 router.post("/manage", adminOrderPanel.updateOrder);
-
 router.get("/coupons", adminCouponPanel.manageCoupon);
 router.get("/edit-coupon", adminCouponPanel.editCoupon);
 router.post("/edit-coupons", adminCouponPanel.editingCoupon);
@@ -65,11 +47,11 @@ router.get("/delete-coupon", adminCouponPanel.deleteCoupon);
 router.get("/addCoupon", adminCouponPanel.addCoupon);
 router.post("/addCoupons", adminCouponPanel.addingCoupon);
 router.post("/CouponExist", adminCouponPanel.CouponExist);
-
 router.get("/orderDetails", adminOrderPanel.orderDetails);
 router.get("/salesReport", adminsalesReportPanel.salesReport);
 router.get("/dashboard", adminsalesReportPanel.dashboard);
-
 router.get("/error", adminOrderPanel.error);
+
+
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,52 +2,34 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const userProfile = require("../controllers/userProfile");
-const multer = require("multer");
-const path = require("path");
-const Product = require("../model/productModel");
-const Category = require("../model/categoryModel");
-const User = require("../model/userModel");
-const Address = require("../model/addressModel");
 const userauth = require("../controllers/userauth");
 const middleware = require("../middlewares/middlewares");
 const cartController = require("../controllers/cartController");
 const userCheckoutOrderControll = require("../controllers/userCheckoutOrderControll");
-const Cart = require("../model/cartModel");
-
 const passport =require("../config/passport");
+
 
 router.get("/", userController.home);
 router.get("/login", middleware.checkSession, userController.login);
-
 router.get("/auth/google", passport.authenticate("google", { scope: ['email', 'profile'] }));
-
 router.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: '/' }), userauth.googleAuth);
-
 router.post("/login", userauth.login);
-
-
 router.get("/signup", middleware.checkSession2, userController.loadRegister);
 router.post("/checkUserExist", userController.checkUserExist);
-
 router.post("/signup",  middleware.userImage, userController.insertUser);
 router.get("/otp", userController.otp);
 router.post("/signup/verify", userController.verifyMail);
-
 router.get("/reset", userController.reset);
 router.post("/reset", userController.resetPass);
 router.post("/resendOtp", userController.resendOtp);
-
 router.post("/reset/verify", userController.changePassword);
 router.post("/reset/new-password", userController.updatePassword);
-
 router.get("/shop", userController.shop);
 router.get("/home", userController.home);
 router.get("/product-details", userController.productDetails);
 router.post("/rate",userController.rating)
 router.get("/reviews",userController.review)
-
 router.get("/myProfile", userProfile.openProfile);
-
 router.get("/myProfile/add-address", userProfile.addAddres);
 router.post("/myProfile/add-address", userProfile.addAddress);
 router.get("/edit-address", userProfile.changeAddress);
@@ -57,53 +39,37 @@ router.post("/edit-address", userProfile.editAddress);
 router.get("/delete-address", userProfile.deleteAddress);
 router.get("/edit-profile", userProfile.editProfile);
 router.post("/edit-profile", userProfile.updateProfile);
-
-
-
 router.get("/add-to-cart", cartController.addToCart);
-
 router.get("/cart", cartController.cart);
 router.get("/incrementItem", cartController.incCart);
 router.get("/decrementItem", cartController.decCart);
-
 router.get("/checkout", cartController.checkout);
 router.post("/checkout", userCheckoutOrderControll.checkout);
 router.post("/createOrder", cartController.createOrder);
 router.post("/use-wallet", userCheckoutOrderControll.useWallet);
 router.get("/remove-wallet",userCheckoutOrderControll.removeWallet)
-
 router.get("/placeOrder", cartController.placeOrder);
-
 router.get("/orderStatus", cartController.orderStatus);
-
 router.get("/advanceSearch", userController.advanceSearch);
-
 router.get("/AddAddress",userCheckoutOrderControll.addAddress);
 router.post("/AddAddress", userCheckoutOrderControll.checkoutAddAddress);
-
 router.get("/editAddress", userCheckoutOrderControll.editAddress);
 router.post("/editAddress", userCheckoutOrderControll.checkoutEditAddress);
 router.get("/deleteAddress", userCheckoutOrderControll.deleteAddress);
 router.post("/cancelOrder", userCheckoutOrderControll.orderCancell);
 router.post("/returnOrder", userCheckoutOrderControll.returnOrder);
-
 router.get("/orderDetails", userCheckoutOrderControll.orderDetails);
 router.get("/invoice", userCheckoutOrderControll.invoice);
-
 router.get("/addWishlist", cartController.addWishlist);
-
 router.get("/myWishlist", cartController.myWishlist);
-
 router.get("/add-cart", cartController.wishlistToAddCart);
-
 router.get("/deleteWishlist", cartController.deleteWishlist);
-
 router.get("/deleteCart", cartController.deleteCart);
 router.post("/applyCoupon", userCheckoutOrderControll.applyCoupon);
 router.get("/removeCoupon", userCheckoutOrderControll.removeCoupon);
 router.post("/repayOrder", userCheckoutOrderControll.repayOrder);
 router.get("/repay", userCheckoutOrderControll.repay);
-
 router.get("/error", userauth.error);
+
 
 module.exports = router;
