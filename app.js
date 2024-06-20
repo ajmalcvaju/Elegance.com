c853d8586b7fa8cec5e8e0b3e707ed437f95c549
ajmalcvaju <ajmalmayanad@gmail.com>
2024-06-20 18:46:02 +0530

    Elegance-final

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const paths=path.join(__dirname,"public")
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static("public"));
 console.log(paths)
 
 
